ad78aeb511ab7f78775a3925b7052fe5da3f6c0d
Yury Novikov <nov8861@yandex.ru>
2020-06-09 03:37:58 +0300

    Поместил все роуты в один файл

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,16 @@
 const router = require('express').Router();
 const usersRouter = require('./users');
 const articlesRouter = require('./articles');
+const signinRouter = require('./signin');
+const signupRouter = require('./signup');
+const { auth } = require('../middlewares/auth');
+
+router.use('/signin', signinRouter);
+router.use('/signup', signupRouter);
+
+router.use(auth);
 
 router.use('/users', usersRouter);
 router.use('/articles', articlesRouter);
 
-module.exports = router; 
+module.exports = router;
