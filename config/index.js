84c6eec95bdf2a049c04fd01a38bad21dac7b007
Yury Novikov <nov8861@yandex.ru>
2020-06-09 03:35:54 +0300

    Добавил подключение к серверу через process.env

--- config/index.js
+++ config/index.js
@@ -2,10 +2,11 @@ const {
   NODE_ENV,
   JWT_SECRET_KEY,
   PORT,
+  SERVER_CONNECT,
 } = process.env;
 
 module.exports = {
   PORT: PORT || 3000,
   JWT_SECRET_KEY: NODE_ENV === 'production' ? JWT_SECRET_KEY : 'dev-secret',
-  SERVER_CONNECT: 'mongodb://localhost:27017/news-explorer-api',
+  SERVER_CONNECT: NODE_ENV === 'production' ? SERVER_CONNECT : 'mongodb://localhost:27017/news-explorer-api',
 };
