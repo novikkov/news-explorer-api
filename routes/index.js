3a0a14b3ecb2c0cd568f7827b7b7091f5cd48618
Yury Novikov <nov8861@yandex.ru>
2020-06-10 17:24:26 +0300

    Добавил роут

--- routes/index.js
+++ routes/index.js
@@ -4,6 +4,7 @@ const articlesRouter = require('./articles');
 const signinRouter = require('./signin');
 const signupRouter = require('./signup');
 const { auth } = require('../middlewares/auth');
+const NotFoundError = require('../errors/not-found-err');
 
 router.use('/signin', signinRouter);
 router.use('/signup', signupRouter);
@@ -12,5 +13,8 @@ router.use(auth);
 
 router.use('/users', usersRouter);
 router.use('/articles', articlesRouter);
+router.use('/', (req, res, next) => {
+  next(new NotFoundError('Запрашиваемый ресурс не найден'));
+});
 
 module.exports = router;
